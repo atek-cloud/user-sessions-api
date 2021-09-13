56941afb39b7a59c16fe85d4fb6cbf73e2f97b80
Paul Frazee <pfrazee@gmail.com>
2021-09-13 15:44:30 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { rpc, createRpcServer } from '@atek-cloud/node-rpc'
+
+export const ID = 'atek.cloud/user-sessions-api'
+
+export interface UserSessionsApi {
+  // Get the current session
+  whoami (): Promise<UserSession>
+
+  // Create a new session
+  login (creds: UserCreds): Promise<UserSession>
+
+  // End the current session
+  logout (): Promise<void>
+}
+
+export class UserSession {
+  isActive: boolean = false;
+  username: string = '';
+  static schema = {
+    type: 'object',
+    properties: {
+      isActive: {type: 'boolean'},
+      username: {type: 'string'},
+    },
+    required: ['isActive']
+  }
+}
+
+export class UserCreds {
+  username: string = '';
+  password: string = '';
+}
+
+export function createClient () {
+  return rpc<UserSessionsApi>(ID)
+}
+
+export function createServer (handlers: any) {
+  return createRpcServer(handlers, {
+    whoami: {
+      response: UserSession
+    },
+    login: {
+      params: [UserCreds],
+      response: UserSession
+    },
+    logout: {}
+  })
+}
+
+export default createClient()
